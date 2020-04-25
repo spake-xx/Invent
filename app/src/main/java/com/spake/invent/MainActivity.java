cfcb528bc9d640324c0a05756ad5f628d8590ab1
Kuba <spaake123@gmail.com>
2020-04-25 21:56:48 +0200

    - dolne menu - wyświetlanie recyclerview z bazy danych

--- app/src/main/java/com/spake/invent/MainActivity.java
+++ app/src/main/java/com/spake/invent/MainActivity.java
@@ -1,99 +1,31 @@
 package com.spake.invent;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-import com.spake.invent.database.AppDatabase;
-import com.spake.invent.database.entity.Item;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
-    Button btnScanBarcode;
-    AppDatabase mDb;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
-
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
-
-        mDb = AppDatabase.getInstance(getApplicationContext());
-
-        for(Item i:mDb.itemDao().loadAllItems()){
-//            Log.i("ITEM: ".i.getName());
-        }
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
-    private void initViews() {
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
-
-        navigationView.bringToFront();
-        setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-
-//        btnScanBarcode = findViewById(R.id.btnScanBarcode);
-//        btnScanBarcode.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-//            case R.id.btnScanBarcode:
-//                startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
-//                break;
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
-        menuItem.setChecked(true);
-        drawerLayout.closeDrawers();
-
-        switch(menuItem.getItemId()){
-            case R.id.nav_scan:
-                Toast.makeText(MainActivity.this, "Kliknięto skan", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-//        mAdapter.setTasks(mDb.itemDao().loadAllItems());
-    }
 }
